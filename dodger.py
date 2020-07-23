85ac6b26ba457f2a1c7f82410604abd5fabbb74f
Thomas Hong <thom.hong@gmail.com>
2020-07-22 21:27:13 -0700

    Change height max

--- dodger.py
+++ dodger.py
@@ -2,7 +2,7 @@ import pygame, random, sys
 from pygame.locals import *
 
 WINDOWWIDTH = 600
-WINDOWHEIGHT = 600
+WINDOWHEIGHT = 700
 TEXTCOLOR = (0, 0, 0)
 BACKGROUNDCOLOR = (255, 255, 255)
 FPS = 60
